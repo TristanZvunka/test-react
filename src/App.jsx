543d586bd82c01c07366b5834d953390b9766b35
TRISTAN ZVUNKA <tristan.zvunka@gmail.com>
2023-10-12 20:22:24 +0200

    add buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,56 @@
 import PokemonCard from "./components/PokemonCard.jsx"
+import { useState } from "react";
 
-PokemonCard.PropTypes = {
-  
-}
+
+
+
+
+
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+function Go(){
+  setPokemonIndex(pokemonIndex + 1 )
+}
+
+function Back(){
+  setPokemonIndex(pokemonIndex - 1)
+}
+
   const pokemonList = [
     {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ]
-
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
+    ];
 
 
   return (
     <div>
-      <PokemonCard name={pokemonList[0].name} img={pokemonList[0].imgSrc}/>
+      <PokemonCard name={pokemonList[pokemonIndex].name} img={pokemonList[pokemonIndex].imgSrc}/>
+      <div className="container">
+      { pokemonIndex > 0 ? <button onClick={Back}> Précédent </button> : "" }
+      { pokemonIndex < pokemonList.length - 1 ? <button onClick={Go}> Suivant </button> : "" }
+      </div>
     </div>
   );
 
